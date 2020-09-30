25d2c47d692117d2a5278404b5cbf97c6f1a6828
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-09-30 14:31:36 -0400

    cors

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 const db = require("./models");
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(cors({
+    credentials: true,
+}));
+
+// var xhr = new XMLHttpRequest();
+// xhr.open('GET', 'http://abc.com/', true);
+// xhr.withCredentials = true;
+// xhr.send(null);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
