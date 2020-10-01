ee53848cbb94a8b7a6357848e0aaa356f819f59d
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-09-30 23:15:34 -0400

    speed

--- server.js
+++ server.js
@@ -9,15 +9,16 @@ const PORT = process.env.PORT || 8080;
 
 const db = require("./models");
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
 app.use(cors({
     credentials: true,
 }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
